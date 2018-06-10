828854ce97a339ea8f9663ac5ddfa2e22b0796ad
Fred Ma <fma1@terpmail.umd.edu>
2018-06-09 22:01:24 -0400

    Implemented decodeString

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,6 @@
 package example
 
 import scala.collection.mutable.Map
-import scala.annotation.tailrec
 
 object Main extends App {
   // (s: weather, t: therapyw) => theeraw
@@ -39,15 +38,44 @@ object Main extends App {
     sortedResultSeq.reduce((acc, curr) => acc + curr)
   }
 
-  // Sum data type
-  sealed trait EncodedString
-  final case class InnerString(str: String) extends EncodedString
-  final case class InnerCode(code: Code) extends EncodedString
+  def decodeStr(str: String): String = {
+    // Assumption: String is not malformed (i.e. string conforms to the context-free grammar given)
+    def decodeStrHelper(str: String): (String, Int) = {
+      val tmpBuf = new StringBuilder()
+      var curIdx = 0 
 
-  case class Code(repeatNum: Int, encodedStr: EncodedString)
+      // Iterate through the string
+      while (curIdx < str.length && str.charAt(curIdx) != ']') {
+        // If the character at the current index is a digit
+        // we get the location of the next opening bracket
+        // and the number of times to repeat the encoded string
+        // We recurse with str starting at the location of
+        // the next opening bracket + 1
+        // Then we just multiply the resulting string by the repeat number
+        // add that to tmpBuf
+        // and start after the index where it found the ending bracket
+        if (str.charAt(curIdx).isDigit) {
+          val curOpenBracket: Int = str.indexOf('[', curIdx)
+          val curRepeatNum: Int = str.substring(curIdx, curOpenBracket).toInt
 
-  @tailrec
-  def createRecursiveCode(str: String) {
+          decodeStrHelper(str.substring(curOpenBracket + 1)) match {
+            case (resultStr, endIdx) =>
+              tmpBuf.append(resultStr * curRepeatNum)
+              curIdx = curOpenBracket + 1 + endIdx + 1
+          }
+        // If it's not a digit that means we just have a string
+        // so we figure out where the string ends
+        // and add the string to tmpBuf
+        } else {
+          val nextIdx = str.indexWhere((char: Char) => !char.isLetter, curIdx)
+          tmpBuf.append(str.substring(curIdx, nextIdx))
+          curIdx = nextIdx
+        }
+      }
 
+      (tmpBuf.toString(), curIdx)
+    }
+
+    decodeStrHelper(str)._1
   }
 }
