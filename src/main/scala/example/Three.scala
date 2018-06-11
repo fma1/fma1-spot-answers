68642218839d1e368c997a75b4807373e1966bbc
Fred Ma <fma1@terpmail.umd.edu>
2018-06-10 20:36:00 -0400

    Changed naming

--- src/main/scala/example/Three.scala
+++ src/main/scala/example/Three.scala
@@ -6,20 +6,20 @@ import scala.collection.immutable.Set
 object Three {
   // Sort of like a global hashtable for all the possible denominations
   // a certain denomination will map to another hashtable which maps
-  // Int -> List[Int]
-  lazy val maps = Map[List[Int], Map[Int, List[Int]]]()
+  // Int -> Set[List[Int]]
+  lazy val maps = Map[List[Int], Map[Int, Set[List[Int]]]]()
 
   def changePossibilities(amount: Int, denominations: List[Int]): MutableSet[List[Int]] = {
     if (maps.get(denominations).isEmpty) {
-      maps.put(denominations, Map[Int, List[Int]]())
+      maps.put(denominations, Map[Int, Set[List[Int]]]())
     }
 
-    val mutableSet: MutableSet[List[Int]] = MutableSet[List[Int]]()
+    val resultSet: MutableSet[List[Int]] = MutableSet[List[Int]]()
 
     def changeHelper(amount: Int, denominations: List[Int], acc: List[Int]): Unit = {
       if (amount >= 0) {
         if (amount == 0) {
-          set.add(acc)
+          resultSet.add(acc)
         } else {
           denominations.foreach(denomination => changeHelper(amount - denomination, denominations, denomination :: acc))
           ()
@@ -29,6 +29,6 @@ object Three {
 
     changeHelper(amount, denominations, List[Int]())
 
-    mutableSet
+    resultSet
   }
 }
