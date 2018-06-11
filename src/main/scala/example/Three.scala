10408d3e129bd68ec7cf5cc24d1cfcf9704fd7cc
Fred Ma <fma1@terpmail.umd.edu>
2018-06-10 20:29:02 -0400

    Finished Question Three with exponential runtime

--- src/main/scala/example/Three.scala
+++ src/main/scala/example/Three.scala
@@ -1,7 +1,34 @@
 package example
 
+import scala.collection.mutable.{Map, Set => MutableSet}
+import scala.collection.immutable.Set
+
 object Three {
-  def changePossibilities(amount: Int, denominations: Array[Int]): List[Array[Int]] = {
-    List[Array[Int]]()
+  // Sort of like a global hashtable for all the possible denominations
+  // a certain denomination will map to another hashtable which maps
+  // Int -> List[Int]
+  lazy val maps = Map[List[Int], Map[Int, List[Int]]]()
+
+  def changePossibilities(amount: Int, denominations: List[Int]): MutableSet[List[Int]] = {
+    if (maps.get(denominations).isEmpty) {
+      maps.put(denominations, Map[Int, List[Int]]())
+    }
+
+    val mutableSet: MutableSet[List[Int]] = MutableSet[List[Int]]()
+
+    def changeHelper(amount: Int, denominations: List[Int], acc: List[Int]): Unit = {
+      if (amount >= 0) {
+        if (amount == 0) {
+          set.add(acc)
+        } else {
+          denominations.foreach(denomination => changeHelper(amount - denomination, denominations, denomination :: acc))
+          ()
+        }
+      }
+    }
+
+    changeHelper(amount, denominations, List[Int]())
+
+    mutableSet
   }
 }
